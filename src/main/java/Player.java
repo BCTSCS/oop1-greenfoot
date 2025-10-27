5e76ebdb5b9124b00905769b6c0e3305a1536eff
dommos23 <155980154+dommos23@users.noreply.github.com>
2025-10-27 14:08:10 -0400

    Add files via upload

---
+++ src/main/java/Player.java
@@ -0,0 +1,17 @@
+public class Player{
+    public String name;
+    public int level;
+
+public Player(){
+    this.name = 'Dominic';
+    this.level = 3;
+}
+
+public String getName(name){
+    return name;
+}
+
+public int getLevel(level){
+    return level;
+}
+}
