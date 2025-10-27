16eb0af3b84f1af628e71dbeba4e832b9f4c7a40
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-10-27 14:08:49 -0400

    Update Vlogger.java

--- src/main/java/Vlogger.java
+++ src/main/java/Vlogger.java
@@ -2,7 +2,8 @@ public class Vlogger {
     private String name;
     private int yearJoined;
     public Vlogger(){
-
+        this.name = "Ravi";
+        this.yearJoined = 2022;
     }
     public String getName(){
         return name;
