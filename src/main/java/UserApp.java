5f4e42c64bf2e5ac8a8400f4f0f7655f24ed83f7
jsbn <78042486+s-tn@users.noreply.github.com>
2025-10-27 14:04:15 -0400

    gb

--- src/main/java/UserApp.java
+++ src/main/java/UserApp.java
@@ -1,1 +1,12 @@
+public class UserApp extends JFrame {
+    private JPanel panel;
+    private Painting painting;
+    private Vlogger vlogger;
+    private Player user;
+    public UserApp() {
 
+    }
+    public static void main(String[] args) {
+        UserApp app = new UserApp();
+    }
+}
