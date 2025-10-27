7b576bb6b8a57abe013321f82f77958c93933cd5
jsbn <78042486+s-tn@users.noreply.github.com>
2025-10-27 14:09:26 -0400

    idek

--- src/main/java/UserApp.java
+++ src/main/java/UserApp.java
@@ -1,10 +1,17 @@
+
+import javax.swing.JPanel;
+
 public class UserApp extends JFrame {
     private JPanel panel;
     private Painting painting;
     private Vlogger vlogger;
     private Player user;
+    
     public UserApp() {
-
+        this.panel = new JPanel();
+        this.painting = new Painting();
+        this.vlogger = new Vlogger();
+        this.user = new Player();
     }
     public static void main(String[] args) {
         UserApp app = new UserApp();
