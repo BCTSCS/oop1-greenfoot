2df753d00d48ef1d5ecfd382c03b026e4284eea2
adri <155979208+adralv26@users.noreply.github.com>
2025-10-27 14:03:26 -0400

    painting

---
+++ src/main/java/Painting.java
@@ -0,0 +1,14 @@
+public class Painting {
+    private String title;
+    private int year;
+    public Painting() {
+        this.title='GreenFoot';
+        this.year=2067;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public int getYear(){
+        return this.year;
+    }
+}
