8a1ed5c2d355027e92802290054928d1d784968a
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-10-27 14:05:14 -0400

    Create Vlogger.java

---
+++ src/main/java/Vlogger.java
@@ -0,0 +1,13 @@
+public class Vlogger {
+    private String name;
+    private int yearJoined;
+    public Vlogger(){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public int getYearJoined(){
+        return yearJoined;
+    }
+}
